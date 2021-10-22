a4b04ba23a0b71304da3ef9161729f3d6bb35a56
NikitaUtiPuti <92986796+NikitaUtiPuti@users.noreply.github.com>
2021-10-22 15:01:59 +0300

    Add files via upload

---
+++ GitTraining/GitTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTraining
+//
+//  Created by Никита Зюзин on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(123)
+    }
+
+
+}
+
